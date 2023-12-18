7fa0b78b25221f3c0120a92984651373861e519a
Gavin Sadler <35090401+GavinSadler@users.noreply.github.com>
2023-12-18 09:29:07 -0500

    Cleaned some stuff up in DataManager.py

--- DataManager.py
+++ DataManager.py
@@ -1,4 +1,3 @@
-
 import base64
 import os
 import json
@@ -8,6 +7,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
+# File to hold all of the userdata
 DATA_FILE_NAME = "userdata.json"
 
 
@@ -23,11 +23,11 @@ def loadFile():
     Returns:
         Any: object deserialized from json
     """
+
     data = None
 
     with open(DATA_FILE_NAME, "r") as fp:
         data = json.load(fp)
-        
 
     return data
 
@@ -38,6 +38,7 @@ def saveFile(data):
     Args:
         data (Any): json-seriable object that will be written to DATA_FILE_NAME file
     """
+
     with open(DATA_FILE_NAME, "w") as fp:
         json.dump(data, fp)
 
@@ -51,10 +52,12 @@ def userExists(email: str):
     Returns:
         Boolean: True if the user exists, False otherwise
     """
-    
+
+    email = email.lower()
+
     data = loadFile()
-    
-    if [user for user in data["users"] if user["email"] == email.lower()]:
+
+    if [user for user in data["users"] if user["email"] == email]:
         return True
 
     return False
@@ -70,24 +73,29 @@ def registerUser(email: str, password: str):
     Returns:
         Boolean: True if registration was a success, False otherwise
     """
-    
+
     email = email.lower()
-    
-    # Hash password using bcrypt and then encode it with base64 so we can store it in json
-    salt = bcrypt.gensalt()    
-    saltB64 = base64.encodebytes(salt).decode()
-    hashedPassword = bcrypt.hashpw(bytes(password.encode()), salt)
-    hashedPasswordB64 = base64.encodebytes(hashedPassword).decode()
-
-    # Construct the new user object
-    newUser = {"email": email, "password": hashedPasswordB64, "salt": saltB64, "data": ""}
-
-    data = loadFile()
 
     # Make sure the user isn't already registered
     if userExists(email):
         print("ERROR: There is already a user registered with that email")
         return False
+
+    # Hash password and salt using bcrypt and then encode it with base64 so we can store it in json
+    salt = bcrypt.gensalt()
+    saltB64 = base64.urlsafe_b64encode(salt).decode()
+    hashedPassword = bcrypt.hashpw(password.encode(), salt)
+    hashedPasswordB64 = base64.urlsafe_b64encode(hashedPassword).decode()
+
+    # Construct the new user object
+    newUser = {
+        "email": email,
+        "password": hashedPasswordB64,
+        "salt": saltB64,
+        "data": "",
+    }
+
+    data = loadFile()
 
     data["users"].append(newUser)
 
@@ -96,11 +104,14 @@ def registerUser(email: str, password: str):
     return True
 
 
-class UserInstance():
-    
+class UserInstance:
+    """
+        UserInstance can manage encrypted userdata for access during runtime
+    """
+
     email: str
     encryptor: Fernet
-    
+
     def __init__(self, email: str, password: str):
         """Initializes a UserInstance to operate on encrypted userdata
 
@@ -112,61 +123,73 @@ class UserInstance():
             RuntimeError: On a email of a user who does not exist
             RuntimeError: On an incorrect password
         """
-        
+
         email = email.lower()
-        
+
         data = loadFile()
-        
+
         # Check to see if the user exists
         if len([user for user in data["users"] if user["email"] == email]) == 0:
             raise RuntimeError("User not found")
-        
+
         user = [user for user in data["users"] if user["email"] == email][0]
 
         # Load hashed password and salt from file
         hashedPasswordB64: str = user["password"]
-        hashedPassword = base64.decodebytes(hashedPasswordB64.encode())
-        
+        hashedPassword = base64.urlsafe_b64decode(hashedPasswordB64.encode())
+
         saltB64: str = user["salt"]
-        salt = base64.decodebytes(saltB64.encode())
-        
+        salt = base64.urlsafe_b64decode(saltB64.encode())
+
         if not bcrypt.checkpw(password.encode(), hashedPassword):
             raise RuntimeError("Incorrect password")
-        
+
         # Generate a key and encryption object to encyrpt/decrypt user data
         kdf = PBKDF2HMAC(hashes.SHA256(), 32, salt, 10000)
         key = kdf.derive(password.encode())
         keyB64 = base64.urlsafe_b64encode(key)
-        
+
         self.encryptor = Fernet(keyB64)
         self.email = email
-    
-    
+
     def getUserData(self):
+        """Returns encrypted user data
+
+        Returns:
+            Any: the decrypted and decoded user data object
+        """
         
         data = loadFile()
-        
-        userDataEncrypted: str = [user for user in data["users"] if user["email"] == self.email][0]["data"]
-        
+
+        userDataEncrypted: str = [
+            user for user in data["users"] if user["email"] == self.email
+        ][0]["data"]
+
         # If user's data field is empty, decryption will fail, so return an empty dict
-        if (userDataEncrypted == ""):
+        if userDataEncrypted == "":
             return {}
-        
+
         rawData = self.encryptor.decrypt(userDataEncrypted)
-        
+
         data = json.loads(rawData)
-        
+
         return data
 
-    
     def setUserData(self, userData):
+        """Encrypts, encodes, and stores the given userData object
+
+        Args:
+            userData (Any): a json-serializable object that will be enecrypted and stored
+        """
         
         data = loadFile()
-        
+
         userDataSerialized = json.dumps(userData)
-        
+
         userDataEncrypted = self.encryptor.encrypt(userDataSerialized.encode())
-        
-        [user for user in data["users"] if user["email"] == self.email][0]["data"] = userDataEncrypted.decode()
-        
+
+        [user for user in data["users"] if user["email"] == self.email][0][
+            "data"
+        ] = userDataEncrypted.decode()
+
         saveFile(data)
